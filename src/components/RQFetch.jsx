ec0ad2f5e31d57392c317c0b09fed2dc83e3ece3
Emmanuel Kumah <mailkumah@gmail.com>
2023-12-28 23:15:28 +0000

    Refactored Reasons.jsx

--- src/components/RQFetch.jsx
+++ src/components/RQFetch.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 //fetch function
 const fetchedUsers = () => {
   return axios.get("http://localhost:4000/users");
 };
+//add user function
+const addUser = (user) => {
+  return axios.post("http://localhost:4000/users", user);
+};
 const RQFetch = () => {
+  const [name, setName] = useState("");
+  const [location, setLocation] = useState("");
+
+  //Querying data
+
   const { isLoading, error, isError, isFetching, data, refetch } = useQuery({
     queryKey: ["users"],
     queryFn: fetchedUsers,
     // refetchOnWindowFocus: true, //fetch data when window focus
-    enabled: false, // disable fetching data automatically
+    // enabled: true, // disable fetching data automatically
+    // refetchOnMount: true // refetch data when component mounts
   });
+
+  //Mutating data
+  const { mutate } = useMutation({ mutationFn: addUser });
 
   if (isLoading) {
     return <span>Loading...</span>;
@@ -24,21 +37,51 @@ const RQFetch = () => {
   if (isFetching) {
     return <p>Refetching data in the background...</p>;
   }
+  // handle form submission
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    const data = { name, location };
+    mutate(data);
 
+    setName("");
+    setLocation("");
+  };
   return (
-    <section className="userContainer">
-      <h3>Display Users</h3>
-      {/* Fetch data on button click */}
-      <button onClick={refetch}>Fetch Data</button>
-      <ul className="users">
-        {data?.data.map((user) => (
-          <li key={user.id}>
-            <h4>{user.name}</h4>
-            <p>{user.location}</p>
-          </li>
-        ))}
-      </ul>
-    </section>
+    <>
+      <section className="userContainer">
+        <h3>Display Users</h3>
+        {/* Fetch data on button click */}
+        <button onClick={refetch}>Fetch Data</button>
+        <ul className="users">
+          {data?.data.map((user) => (
+            <li key={user.id}>
+              <h4>{user.name}</h4>
+              <p>{user.location}</p>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <section>
+        <h3>Add New user</h3>
+        <form onSubmit={handleFormSubmission}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter full name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            name="location"
+            placeholder="Enter the location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+          <button>Submit</button>
+        </form>
+      </section>
+    </>
   );
 };
 
