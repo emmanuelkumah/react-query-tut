6028b466aaed66c91c32bb74a496baf9c6bc8efd
Emmanuel Kumah <mailkumah@gmail.com>
2023-12-28 19:10:00 +0000

    Demo on fetching queries

--- src/components/RQFetch.jsx
+++ src/components/RQFetch.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import axios from "axios";
-import { useQuery } from "react-query";
+import { useQuery } from "@tanstack/react-query";
 
 //fetch function
 const fetchedUsers = () => {
-  return axios.get("http://localhost:4000/ers"); // altered api endpoint
+  return axios.get("http://localhost:4000/users");
 };
 const RQFetch = () => {
-  const { isLoading, error, isError, data } = useQuery("users", fetchedUsers);
+  const { isLoading, error, isError, isFetching, data, refetch } = useQuery({
+    queryKey: ["users"],
+    queryFn: fetchedUsers,
+    // refetchOnWindowFocus: true, //fetch data when window focus
+    enabled: false, // disable fetching data automatically
+  });
+
   if (isLoading) {
     return <span>Loading...</span>;
   }
@@ -15,12 +21,17 @@ const RQFetch = () => {
   if (isError) {
     return <p>Error loading data :${error.message}</p>;
   }
+  if (isFetching) {
+    return <p>Refetching data in the background...</p>;
+  }
 
   return (
     <section className="userContainer">
       <h3>Display Users</h3>
+      {/* Fetch data on button click */}
+      <button onClick={refetch}>Fetch Data</button>
       <ul className="users">
-        {data.data.map((user) => (
+        {data?.data.map((user) => (
           <li key={user.id}>
             <h4>{user.name}</h4>
             <p>{user.location}</p>
--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,9 @@ import Root from "./routes/root.jsx";
 import ErrorPage from "./error-page.jsx";
 import RQFetch from "./components/RQFetch.jsx";
 import Home from "../src/components/Home.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import { QueryClientProvider, QueryClient } from "react-query";
-import { ReactQueryDevTools } from "react-query/devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const router = createBrowserRouter([
   {
@@ -30,11 +30,12 @@ const router = createBrowserRouter([
 ]);
 //create new instance of queryClient
 const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
-      <ReactQueryDevTools initialIsOpen={false} position="bottom-right" />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   </React.StrictMode>
 );
