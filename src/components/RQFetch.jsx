2ca13a4fee9aea6a6068ad359098238c6aa0e76f
Emmanuel Kumah <mailkumah@gmail.com>
2024-01-02 23:10:34 +0000

    Complete basic query fetching in RQFetch.jsx

--- src/components/RQFetch.jsx
+++ src/components/RQFetch.jsx
@@ -1,85 +1,38 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { useQuery, useMutation } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 
 //fetch function
 const fetchedUsers = () => {
   return axios.get("http://localhost:4000/users");
 };
-//add user function
-const addUser = (user) => {
-  return axios.post("http://localhost:4000/users", user);
-};
+
 const RQFetch = () => {
-  const [name, setName] = useState("");
-  const [location, setLocation] = useState("");
-
-  //Querying data
-
-  const { isLoading, error, isError, isFetching, data, refetch } = useQuery({
+  const { data, isError, isLoading, error } = useQuery({
     queryKey: ["users"],
     queryFn: fetchedUsers,
-    // refetchOnWindowFocus: true, //fetch data when window focus
-    // enabled: true, // disable fetching data automatically
-    // refetchOnMount: true // refetch data when component mounts
   });
-
-  //Mutating data
-  const { mutate } = useMutation({ mutationFn: addUser });
-
   if (isLoading) {
-    return <span>Loading...</span>;
+    return <h3>Loading data ... </h3>;
   }
 
   if (isError) {
-    return <p>Error loading data :${error.message}</p>;
+    return <h3>`Error loading data ${error.message}`</h3>;
   }
-  if (isFetching) {
-    return <p>Refetching data in the background...</p>;
-  }
-  // handle form submission
-  const handleFormSubmission = (e) => {
-    e.preventDefault();
-    const data = { name, location };
-    mutate(data);
-
-    setName("");
-    setLocation("");
-  };
   return (
     <>
       <section className="userContainer">
         <h3>Display Users</h3>
-        {/* Fetch data on button click */}
-        <button onClick={refetch}>Fetch Data</button>
-        <ul className="users">
-          {data?.data.map((user) => (
-            <li key={user.id}>
-              <h4>{user.name}</h4>
-              <p>{user.location}</p>
-            </li>
-          ))}
+        <ul>
+          {data.data.map((user) => {
+            return (
+              <li key={user.id}>
+                <h4>{user.name}</h4>
+                <p>{user.location}</p>
+              </li>
+            );
+          })}
         </ul>
-      </section>
-      <section>
-        <h3>Add New user</h3>
-        <form onSubmit={handleFormSubmission}>
-          <input
-            type="text"
-            name="name"
-            placeholder="Enter full name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-          <input
-            type="text"
-            name="location"
-            placeholder="Enter the location"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
-          <button>Submit</button>
-        </form>
       </section>
     </>
   );
